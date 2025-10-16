5f27d0a0d95458cda26c42ee41dffdf71d0af044
Diego Blanco <diego_blanco@msn.com>
2025-10-16 19:58:58 +0000

    Adiciona exercício 2 da lista 5

---
+++ Exercicios_ALG/Lista5/Ex02_MediaPresenca.java
@@ -0,0 +1,23 @@
+package Lista5;
+
+import java.util.Scanner;
+
+public class Ex02_MediaPresenca {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite as notas do aluno: ");
+        double soma = sc.nextDouble() + sc.nextDouble() + sc.nextDouble() + sc.nextDouble();
+
+        System.out.print("Digite a presenca do aluno (em %): ");
+        int freq = sc.nextInt();
+
+        System.out.printf(
+            "Média: %.1f - %s%n",
+            (soma / 4.0),
+            ((soma / 4.0) >= 6.0 && freq >= 75) ? "Aprovado" : "Reprovado"
+        );
+
+        sc.close();
+    }
+}
