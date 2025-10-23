7cfee521a3e2c16d8d45e996d30fd5690a29e797
Diego Blanco <diego_blanco@msn.com>
2025-10-23 19:40:02 +0000

    Adiciona o exercício 2 da lista 6

---
+++ Exercicios_ALG/Lista6/Ex2Triangulos.java
@@ -0,0 +1,27 @@
+package Lista6;
+import java.util.Scanner;
+
+public class Ex2Triangulos {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.print("Digite o valor dos lados do triângulo: ");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        double c = sc.nextDouble();
+
+        if ((a + b > c) && (b + c > a) && (c + a > b) ) {
+            if (a == b && b == c) {
+                System.out.println("Triângulo equilátero");
+            } else if (a == b || b == c || a == c) {
+                System.out.println("Triângulo isósceles");
+            } else {
+                System.out.println("Triângulo escaleno");
+            }
+        } else {
+            System.out.println("Triângulo inválido");
+        }
+
+        sc.close();
+    }
+}
