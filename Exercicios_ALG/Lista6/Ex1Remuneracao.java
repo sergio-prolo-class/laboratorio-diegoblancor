b54be88488d5e6a87110a1e70e69186fa9821c59
Diego Blanco <diego_blanco@msn.com>
2025-10-23 18:58:12 +0000

    Ex1 - Calculo da remuneração do vendedor

---
+++ Exercicios_ALG/Lista6/Ex1Remuneracao.java
@@ -0,0 +1,29 @@
+package Lista6;
+import java.util.Scanner;
+
+public class Ex1Remuneracao {
+    public static void main (String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+       System.out.print("Digite o valor total de vendas (R$) : ");
+        double vendas = sc.nextDouble();
+
+        System.out.print("Digite a nota da avaliação de desempenho (1-10): ");
+        int avaliacao = sc.nextInt();
+
+        double salarioFixo = 2000.0;
+        double taxa = (vendas >= 10000.0) ? 0.07 : 0.05;
+        double comissao = vendas * taxa;
+
+        double bonus = 0.0;
+        if (avaliacao >= 8) {
+            bonus = 500.0;
+        }
+
+        double remuneracao = salarioFixo + comissao + bonus;
+
+        System.out.printf("Remuneração: R$ %.2f%n", remuneracao);
+
+        sc.close();
+    }
+}
